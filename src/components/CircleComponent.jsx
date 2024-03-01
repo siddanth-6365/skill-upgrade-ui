bda4fec9b53c0afc560a1872b1666b0463788868
siddanth reddy <siddanthreddy@siddanths-MacBook-Air.local>
2024-03-02 02:46:32 +0530

    fixed import error

--- src/components/CircleComponent.jsx
+++ src/components/CircleComponent.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import AboutUs from "./AboutUs";
+import AboutUs from "./Aboutus";
 
 const CircleComponent = () => {
   useEffect(() => {
