08c5e579165199c9d45c6fd9c63271d0e72168aa
ganesh-7985 <shankarganesh3005@gmail.com>
2024-03-07 13:50:10 +0530

    added specific page

--- src/components/CoursePage.jsx
+++ src/components/CoursePage.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Heading from "./ui/heading";
-import courseData from "@/data/homeCourses.json";
 import { Card } from "./ui/Card";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import courseData from '@/data/courseData.json'
 
 const courseVariant = {
   visible: { x: 0, opacity: 1, transition: { duration: 0.5 } },
@@ -35,7 +35,7 @@ const CoursePage = () => {
         <motion.div className=" mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {courses &&
             courses.map((course) => {
-              return <Card card={course} key={course.id} isThree={true} />;
+              return <Card card={course} key={course.id}/>;
             })}
         </motion.div>
       </div>
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
-export const Card = ({ card, key, isThree }) => {
+export const Card = ({ card, key }) => {
+  const truncatedDescription =
+  card.description.length > 220
+    ? card.description.slice(0, 200) + "..."
+    : card.description;
+
   return (
     <>
       <div
@@ -9,16 +15,19 @@ export const Card = ({ card, key, isThree }) => {
       >
         <img
           className="object-cover object-center w-full h-64 rounded-lg"
-          src={card.imgUrl}
+          src={card.backgroundImage}
           alt={card.title}
         />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{card.title}</div>
-          <p className="text-gray-700 text-base">{card.description}</p>
-          <div className="mt-4 text-center">
-            <button className="bg-[#7c51f0] hover:bg-[#6c46d5] text-white font-bold py-2 px-4 rounded">
-              Buy Now
-            </button>
+          <p className="text-gray-700 h-32 text-base">{truncatedDescription}</p>
+          <div className=" flex items-center justify-evenly gap-5 mt-4 text-center">
+            <Link href={`/courses?id=${card.id}`}><button className="bg-[#7c51f0] hover:bg-[#6c46d5] text-white font-bold py-2 px-4 rounded">
+              Read More
+            </button></Link>
+          
+            <h6 className="font-semibold text-xl">{card.price}</h6>
+            
           </div>
         </div>
       </div>
