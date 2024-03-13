c1564bb0747658c829bda68f950dc9f9bd8933cc
siddanth reddy <siddanthreddy@siddanths-MacBook-Air.local>
2024-03-13 20:06:57 +0530

    minor typo

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -6,7 +6,7 @@ export default function Example() {
       <div id="instructors" className="bg-white py-4 sm:py-[5rem] " style={{ fontFamily: "Cambria, Georgia, serif" }}>
         <div className="mx-auto  max-w-7xl  px-6 lg:px-8 ">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Meet our Instructors
+            Know about your Instructors
           </h2>
           <p className="mt-6 text-xl leading-8 text-gray-600">
             Our team consists of experienced professionals with diverse
