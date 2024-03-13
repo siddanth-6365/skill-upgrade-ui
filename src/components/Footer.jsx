36587a08e221c84c81461e78b7f0585dee93fdbd
siddanth reddy <siddanthreddy@siddanths-MacBook-Air.local>
2024-03-13 10:26:32 +0530

    added social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
               © 2024 SkillUpgrade - All Rights Reserved
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-              {/* linkedin */}
+              {/* linkedin 
               <a
                 className="ml-3 text-gray-500"
                 href="https://www.linkedin.com/in/skill-upgrade-6883a22ba?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
@@ -64,7 +64,7 @@ const Footer = () => {
                     d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"
                   ></path>
                 </svg>
-              </a>
+              </a> */}
               {/* telegram */}
               <a
                 className="ml-3 text-gray-500"
